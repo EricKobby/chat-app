eeed1461ac481251142a20fd700f0f43c3fb13d1
Eric Kobby Mensah <mensaheric743@hotmail.com>
2021-06-03 02:55:30 +0000

    updates to .env files

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const baseUrl = process.env.REACT_APP_API_URL as string
 export async function getChatMessages(currentUser: string, selectedUser: string) {
-    return await axios.get(`/messages/${currentUser}/${selectedUser}`)
+    return await axios.get(baseUrl.concat(`/messages/${currentUser}/${selectedUser}`))
 }
