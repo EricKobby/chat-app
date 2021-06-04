36a139fcde416f182c26b4f5f4fd64b9d2ce4aff
Eric Kobby Mensah <mensaheric743@hotmail.com>
2021-06-04 16:15:27 +0000

    updated ngrok routes

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 const baseUrl = process.env.REACT_APP_API_URL as string
-export async function getChatMessages(currentUser: string, selectedUser: string) {
+export async function getChatMessages(currentUser: string, selectedUser: string): Promise<AxiosResponse<any>> {
     return await axios.get(baseUrl.concat(`/messages/${currentUser}/${selectedUser}`))
 }
